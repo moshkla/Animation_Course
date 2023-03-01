45ca9fce9e326076a4195910201786b1edf7a72f
Anas Abd Elazim <anas.ss@yahoo.com>
2023-03-01 13:08:09 +0200

    Example 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
 
 void main() {
   runApp(const MyApp());
@@ -15,18 +16,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Example2(),
+      home: const Example3(),
     );
   }
 }
@@ -38,7 +30,8 @@ class Example1 extends StatefulWidget {
   State<Example1> createState() => _Example1State();
 }
 
-class _Example1State extends State<Example1> with SingleTickerProviderStateMixin {
+class _Example1State extends State<Example1>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
 
@@ -95,8 +88,7 @@ class Example2 extends StatefulWidget {
   State<Example2> createState() => _Example2State();
 }
 
-class _Example2State extends State<Example2>
-    with TickerProviderStateMixin {
+class _Example2State extends State<Example2> with TickerProviderStateMixin {
   late AnimationController _counterClockwiseRotationController;
   late Animation<double> _counterClockwiseRotationAnimation;
   late AnimationController _flipController;
@@ -164,7 +156,7 @@ class _Example2State extends State<Example2>
     });
 
     _flipController.addStatusListener(
-          (status) {
+      (status) {
         if (status == AnimationStatus.completed) {
           _counterClockwiseRotationAnimation = Tween<double>(
             begin: _counterClockwiseRotationAnimation.value,
@@ -293,4 +285,141 @@ class HalfCircleClipper extends CustomClipper<Path> {
 
 extension on VoidCallback {
   Future<void> delayed(Duration duration) => Future.delayed(duration, this);
+}
+
+const widthAndHeight = 100.0;
+
+class Example3 extends StatefulWidget {
+  const Example3({Key? key}) : super(key: key);
+
+  @override
+  State<Example3> createState() => _Example3State();
+}
+
+class _Example3State extends State<Example3> with TickerProviderStateMixin {
+  late AnimationController _xController;
+  late AnimationController _yController;
+  late AnimationController _zController;
+  late Tween<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _xController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20));
+    _yController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 30));
+    _zController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 40));
+
+    _animation = Tween<double>(begin: 0, end: 2 * pi);
+  }
+
+  @override
+  void dispose() {
+    _xController.dispose();
+    _yController.dispose();
+    _zController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _xController
+      ..reset()
+      ..repeat();
+    _yController
+      ..reset()
+      ..repeat();
+    _zController
+      ..reset()
+      ..repeat();
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 100,
+            width: double.infinity,
+          ),
+          AnimatedBuilder(
+              animation: Listenable.merge([
+                _xController,
+                _yController,
+                _zController,
+              ]),
+              builder: (context, child) {
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..rotateX(_animation.evaluate(_xController))
+                    ..rotateY(_animation.evaluate(_yController))
+                    ..rotateZ(_animation.evaluate(_zController)),
+                  child: Stack(
+                    children: [
+                      //front
+
+                      //back
+                      Transform(
+                        transform: Matrix4.identity()
+                          ..translate(Vector3(0, 0, -widthAndHeight)),
+                        child: Container(
+                          color: Colors.green,
+                          height: widthAndHeight,
+                          width: widthAndHeight,
+                        ),
+                      ),
+
+                      //side left
+                      Transform(
+                        alignment: Alignment.centerLeft,
+                        transform: Matrix4.identity()..rotateY(pi / 2),
+                        child: Container(
+                          color: Colors.yellow,
+                          height: widthAndHeight,
+                          width: widthAndHeight,
+                        ),
+                      ),
+                      Transform(
+                        alignment: Alignment.centerRight,
+                        transform: Matrix4.identity()..rotateY(-pi / 2),
+                        child: Container(
+                          color: Colors.purple,
+                          height: widthAndHeight,
+                          width: widthAndHeight,
+                        ),
+                      ),
+                      //front
+                      Container(
+                        color: Colors.red,
+                        height: widthAndHeight,
+                        width: widthAndHeight,
+                      ),
+                      //top
+                      Transform(
+                        alignment: Alignment.topCenter,
+                        transform: Matrix4.identity()..rotateX(-pi / 2),
+                        child: Container(
+                          color: Colors.blue,
+                          height: widthAndHeight,
+                          width: widthAndHeight,
+                        ),
+                      ),
+                      //bottom
+                      Transform(
+                        alignment: Alignment.bottomCenter,
+                        transform: Matrix4.identity()..rotateX(pi / 2),
+                        child: Container(
+                          color: Colors.black,
+                          height: widthAndHeight,
+                          width: widthAndHeight,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ],
+      ),
+    );
+  }
 }
